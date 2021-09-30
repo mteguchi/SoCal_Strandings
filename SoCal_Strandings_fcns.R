981e05586573296b069d10fc4c53964aadf91a3b
mteguchi <tomo.eguchi@noaa.gov>
2021-09-29 22:11:33 -0700

    New models and streamlinig with a new function

--- SoCal_Strandings_fcns.R
+++ SoCal_Strandings_fcns.R
@@ -1,4 +1,34 @@
 
+# runs one model 
+run.model <- function(parameters.list, jags.data, model.name, MCMC.params){
+  
+  if (!file.exists(paste0("RData/jags_out_", model.name, ".rds"))){
+    jm <- jags(data = jags.data,
+               #inits = inits,
+               parameters.to.save= parameters.list,
+               model.file = paste0("models/model_DcStranding_", model.name, ".txt"),
+               n.chains = MCMC.params$n.chains,
+               n.burnin = MCMC.params$n.burnin,
+               n.thin = MCMC.params$n.thin,
+               n.iter = MCMC.params$n.samples,
+               DIC = T, 
+               parallel=T)
+    
+    saveRDS(jm, file = paste0("RData/jags_out_", model.name, ".rds"))
+    
+  } else {
+    
+    jm <- readRDS(file = paste0("RData/jags_out_", model.name, ".rds"))
+  }
+  
+  
+  jm.MCMC <- MCMC.diag(jm = jm, MCMC.params = MCMC.params)
+  
+  return(list(jm.out = jm,
+              jm.MCMC = jm.MCMC))  
+}
+
+
 # extract MCMC diagnostic statistics, including Rhat, loglikelihood, DIC, and LOOIC
 MCMC.diag <- function(jm, MCMC.params){
   n.per.chain <- (MCMC.params$n.samples - MCMC.params$n.burnin)/MCMC.params$n.thin
